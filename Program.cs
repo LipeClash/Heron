1c773f459f7f6bed9f79864313fa2a22a309130d
LipeClash <felipeclaudino2003@hotmail.com>
2020-05-21 22:49:36 -0300

    feito

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Heron
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("         |\\                                               ");
+            Console.WriteLine("         | \\                       SEMIPERÍMETRO          ");
+            Console.WriteLine("         |  \\                                             ");
+            Console.WriteLine("         |   \\                       a + b + c            ");
+            Console.WriteLine("         |    \\                S =   __________           ");
+            Console.WriteLine("         |     \\                         7                ");
+            Console.WriteLine("         |      \\                                         ");
+            Console.WriteLine("  a = 5  |       \\    b = 7           ARÉA                ");
+            Console.WriteLine("         |        \\                                       ");
+            Console.WriteLine("         |         \\                   2.5                ");
+            Console.WriteLine("         |          \\          A =    _____  =  10        ");
+            Console.WriteLine("         |           \\                  2                 ");
+            Console.WriteLine("         |____________\\                                   ");
+            Console.WriteLine("             c = 2                      10                 ");
+            Console.WriteLine("                                       _____  =  5         ");
+            Console.WriteLine("                                         2                 ");
+            
+            Double A = 5;
+            Double B = 7;
+            Double C = 2;
+            Double Medidasdolados = A + B + C;
+            Console.WriteLine($" Lado a..: {A} \n Lado b..: {B} \n Lado c..: {C}");
+            Console.WriteLine("\n");
+            Console.WriteLine(" Semiperímetro..: 7");
+            Console.WriteLine(" Área...........: 5");
+
+        }
+    }
+}
